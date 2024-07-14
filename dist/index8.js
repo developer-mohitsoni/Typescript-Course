1d887100717c2a2f6f7216b59f1d607425e01d50
Mohit Soni <developer_mohitsoni@outlook.com>
2024-07-14 18:00:46 +0530

    Type Assertion Part-1

---
+++ dist/index8.js
@@ -0,0 +1,11 @@
+"use strict";
+//! Type Assertion
+const btn = document.getElementById("btn");
+// OR
+//* const btn = <HTMLElement>document.getElementById("btn");
+// OR
+// const btn = document.getElementById(#btn")!;
+btn.onclick;
+//! Another Example:-
+const img = document.getElementById("myImg");
+img.src;
---
+++ src/index8.ts
@@ -0,0 +1,18 @@
+//! Type Assertion
+
+const btn = document.getElementById("btn") as HTMLElement;
+
+// OR
+
+//* const btn = <HTMLElement>document.getElementById("btn");
+
+// OR
+
+// const btn = document.getElementById(#btn")!;
+
+btn.onclick;
+
+//! Another Example:-
+
+const img = document.getElementById("myImg") as HTMLImageElement;
+img.src
